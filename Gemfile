88e412de847aedf8fe450eb731a0b8729204a1d0
Igi <liska.igor4@gmail.com>
2012-09-27 17:57:47 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "http://rubygems.org"
+
+gem "thin", "~>1.5.0"
+gem 'activesupport', "~>3.2.8", :require => "active_support/all"
+gem "activerecord", "~>3.2.8", :require => "active_record"
+gem 'mysql2', "~>0.3.11"
---
+++ test.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+Thread::abort_on_exception = true
+ActiveRecord::Base.establish_connection(YAML.load(File.read("#{File.dirname(__FILE__)}/database.yml")))
+
+class Customer < ActiveRecord::Base
+  has_many :invoices
+  
+  # id : Integer, auto-increment
+  # name : String
+  
+  def last_invoice
+    self.invoices.order('created_at DESC').first
+  end
+end
+
+class Invoice < ActiveRecord::Base
+  belongs_to :customer
+  
+  # id : Integer, auto-increment
+  # customer_id : Integer
+  # created_at : Datetime
+end
+
+5.times do
+  Thread.new do
+    loop do
+      ActiveRecord::Base.connection_pool.with_connection do
+        Customer.all.each do |customer|
+          invoice = customer.last_invoice
+          puts "Last invoice for customer #{customer.name} was at #{invoice.nil? ? "never" : invoice.created_at}"
+        end
+      end
+      
+      sleep 5
+    end
+  end
+end
+
+puts "goes to sleep"
+sleep 60
